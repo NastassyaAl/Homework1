6b6bbdd1529c15ba8fd2a8a84f5658ec990d7c64
unknown <nastena-alhovik@yandex.ru>
2022-09-02 19:53:48 +0300

    add Task 2 homework 1

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("a= ");
+string first = Console.ReadLine();
+int a = Convert.ToInt32(first);
+Console.Write("b= ");
+string second = Console.ReadLine();
+int b = Convert.ToInt32(second);
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+Console.Write("max= ");
+Console.WriteLine(max);
