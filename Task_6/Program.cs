301165879cbbbb95828ef817d5191aa297c929a0
unknown <nastena-alhovik@yandex.ru>
2022-09-03 18:10:44 +0300

    add Task 6 Homework 1

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int number = Convert.ToInt32(a);
+
+if (number %2 == 0)
+{
+    Console.WriteLine("Да, число четное");
+}
+else
+{
+    Console.WriteLine("Нет, число нечетное");
+}
