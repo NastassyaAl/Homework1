4097873f535ce0b00bbae8a407ea3e1388dc1b8a
unknown <nastena-alhovik@yandex.ru>
2022-09-03 18:44:08 +0300

    add Task 8 Homework 1

---
+++ Task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("N= ");
+string a = Console.ReadLine();
+int N = Convert.ToInt32(a);
+
+int n = 1;
+
+while (n < N)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + ",");
+    }
+    n++;
+}
